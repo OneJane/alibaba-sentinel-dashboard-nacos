5da9ecdf0bd79c3c91174f3e70cb166109957b86
OneJane <15806204096@163.com>
2019-08-09 09:27:03 +0800

    flow+degrade

---
+++ src/main/java/com/alibaba/csp/sentinel/dashboard/rule/nacos/NacosConfigConstant.java
@@ -0,0 +1,13 @@
+package com.alibaba.csp.sentinel.dashboard.rule.nacos;
+
+public final class NacosConfigConstant {
+
+    public static final String GROUP_ID = "DEFAULT_GROUP";
+
+    public static final String FLOW_DATA_ID_POSTFIX = "-flow-rules";
+    public static final String PARAM_FLOW_DATA_ID_POSTFIX = "-param-flow-rules";
+    public static final String DEGRADE_DATA_ID_POSTFIX = "-degrade-rules";
+    public static final String SYSTEM_DATA_ID_POSTFIX = "-system-rules";
+    public static final String AUTHORITY_DATA_ID_POSTFIX = "-authority-rules";
+
+}
